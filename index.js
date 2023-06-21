d602d127d59bdd64961a533164d0e1da9f29468e
AASHI GUPTA <aashiyash12@gmail.com>
2023-06-22 00:31:13 +0530

    hi

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
