a1fac96d37b483a51312a9189f11e324328505c9
AASHI GUPTA <aashiyash12@gmail.com>
2023-06-21 20:38:13 +0530

    chatAppGroup

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('join', (room) => {
+    socket.join(room);
+    socket.to(room).emit('message', 'A new user has joined the room');
+  });
+
+  socket.on('message', (message) => {
+    const { room, text } = message;
+    io.to(room).emit('message', text);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
